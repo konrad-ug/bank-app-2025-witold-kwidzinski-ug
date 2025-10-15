0358106f1014cdd34d969ddcdef3feb2b571d456
Witold <w.kwidzinski.227@studms.ug.edu.pl>
2025-10-15 16:00:47 +0200

    added features 2-4

--- tests/unit/test_account_create.py
+++ tests/unit/test_account_create.py
@@ -3,7 +3,32 @@ from src.account import Account
 
 class TestAccount:
     def test_account_creation(self):
-        account = Account("John", "Doe")
+        account = Account("John", "Doe", "12345678909")
         assert account.first_name == "John"
         assert account.last_name == "Doe"
-        assert account.balance == 0
+        assert account.balance == 0.0
+        assert account.pesel == "12345678909"
+
+    def test_pesel_too_long(self):
+        account = Account("Joe", "Dohn", "1234567890987")
+        assert account.pesel == "Invalid"
+
+    def test_pesel_too_short(self):
+        account = Account("Joe", "Dohn", "1234587")
+        assert account.pesel == "Invalid"
+
+    def test_pesel_empty(self):
+        account = Account("Joe", "Dohn", "")
+        assert account.pesel == "Invalid"
+
+    def test_promo_good(self):
+        account = Account("John", "Doe", "12345678909", "PROM_123")
+        assert account.balance == 50.0
+
+    def test_promo_length(self):
+        account = Account("John", "Doe", "12345678909", "PROM_12453")
+        assert account.balance == 0.0
+
+    def test_promo_correct_prefix(self):
+        account = Account("John", "Doe", "12345678909", "PORM_153")
+        assert account.balance == 0.0
